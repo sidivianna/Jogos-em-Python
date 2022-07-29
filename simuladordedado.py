b1293b53a6c44cc7d99b0490da382c1090eb9584
sidivianna <sidineirochavianna19@gmail.com>
2022-07-29 10:48:04 -0300

    commit inicial

---
+++ simuladordedado.py
@@ -0,0 +1,29 @@
+
+#Simulador de Dado
+
+import random
+
+class SimuladorDeDado:
+    def __init__(self):
+        self.valor_minimo = 1
+        self.valor_maximo = 6
+        self.mensagem = 'Você gostaria de gerar um novo valor para o dado?'
+
+    def Iniciar(self):
+        resposta = input(self.mensagem)
+        try:
+            if resposta == 'sim' or resposta == 's':
+                self.GerarValorDoDado()
+            elif resposta == 'não' or resposta == 'n':
+                print('Agradecemos sua participação!')
+            else:
+                print('Favor digitar sim ou não!')
+        except:
+            print('Ocorreu um erro ao receber sua resposta.')
+
+
+    def GerarValorDoDado(self):
+        print(random.randint(self.valor_minimo, self.valor_maximo))
+
+simulador = SimuladorDeDado()
+simulador.Iniciar()
